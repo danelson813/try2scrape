e3a0a86694be4e2d5df4f56088f2966893aabb87
Dan Nelson <44625150+danelson813@users.noreply.github.com>
2025-07-07 21:29:16 -0700

    corrected some formating

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ from loguru import logger
 results = []
 for i in range(1, 11):
     url = f"https://quotes.toscrape.com/page/{i}/"
-    logger.info(f'url: {url}')
+    logger.info(f"url: {url}")
     ua = UserAgent()
-    headers =  {"user-agent": ua.random}
+    headers = {"user-agent": ua.random}
     page = requests.get(url, headers=headers)
-    soup = bs(page.content, 'html.parser')
+    soup = bs(page.content, "html.parser")
 
-    quotes = soup.find_all('div', class_='quote')
+    quotes = soup.find_all("div", class_="quote")
     for quote in quotes:
         result = {
-            'item': quote.find('span', class_='text').text,
-            'author': quote.find('small', class_='author').text,
-            'tags': [item.text for item in quote.find_all('a', class_='tag')]
+            "item": quote.find("span", class_="text").text,
+            "author": quote.find("small", class_="author").text,
+            "tags": [item.text for item in quote.find_all("a", class_="tag")],
         }
         print(result)
         results.append(result)
 data = pd.DataFrame(results)
-data.to_csv('data/results.csv', index=False)
+data.to_csv("data/results.csv", index=False)
